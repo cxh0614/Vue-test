705ba07ece29ed6b021b4406309eca079cfd6344
Ly <626947388@qq.com>
2019-04-17 20:24:48 +0800

    初始化项目 & 建立组件 & 完成基本页面

---
+++ VueDemo/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="root">
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  components: {
+     Header, Main, Footer
+  }
+}
+</script>
+
+<style>
+  .todo-container {
+    width: 600px;
+    margin: 0 auto;
+  }
+  .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+</style>
